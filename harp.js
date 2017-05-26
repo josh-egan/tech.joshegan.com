745428489e3d60fe1085c6b2f48b300141371a65
Joshua Egan <joshuae@ifit.com>
2017-05-26 11:13:02 -0600

    use newly modified harp and add momemnt!

---
+++ harp.js
@@ -0,0 +1,9 @@
+module.exports = {
+  globals: {
+    "site_title": "tech.joshegan.com",
+    "description": "tech.joshegan.com is a blog about tech adventures.",
+    "site_url": process.env.SITE_URL,
+    "site_environment": process.env.SITE_ENVIRONMENT,
+    moment: require('moment')
+  }
+}
